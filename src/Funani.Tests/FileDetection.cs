2a7c82bfc71983282a3bfd5fb665fad965c66065
Jaap de Haan <jaap.dehaan@color-of-code.de>
2013-01-30 22:49:05 +0100

    Added a test project

---
+++ src/Funani.Tests/FileDetection.cs
@@ -0,0 +1,28 @@
+﻿
+using System;
+using NUnit.Framework;
+
+namespace Funani.Tests
+{
+	[TestFixture]
+	public class FileDetection
+	{
+		[Test]
+		public void TestMethod()
+		{
+			// TODO: Add your test.
+		}
+		
+		[TestFixtureSetUp]
+		public void Init()
+		{
+			// TODO: Add Init code.
+		}
+		
+		[TestFixtureTearDown]
+		public void Dispose()
+		{
+			// TODO: Add tear down code.
+		}
+	}
+}
