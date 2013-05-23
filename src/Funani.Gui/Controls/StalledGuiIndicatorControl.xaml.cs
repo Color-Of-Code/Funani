d39ebad51cb762eb13bdb214ca34b7b6bf44a937
Jaap de Haan <jaap.dehaan@color-of-code.de>
2013-05-23 23:32:21 +0200

    Put the GUI stalled indicator (rotating rectangle) in its own control StalledGuiIndicatorControl

---
+++ src/Funani.Gui/Controls/StalledGuiIndicatorControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Funani.Gui.Controls
+{
+    /// <summary>
+    /// Interaction logic for StalledGuiIndicatorControl.xaml
+    /// </summary>
+    public partial class StalledGuiIndicatorControl : UserControl
+    {
+        public StalledGuiIndicatorControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
