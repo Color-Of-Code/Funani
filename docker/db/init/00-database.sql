21b6e2b40181490a83e08ba67119347f5e4917cf
Jaap de Haan <jaap.dehaan@alpine.de>
2020-10-17 23:54:04 +0200

    fix: added some tables

--- docker/db/init/00-database.sql
+++ docker/db/init/00-database.sql
@@ -1,23 +1,106 @@
 \connect funani;
 
+CREATE extension IF NOT EXISTS "pgcrypto";
+
 /*Create user table in public schema*/
 CREATE TABLE public.user (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY default gen_random_uuid(),
     username TEXT,
+    firstname TEXT,
+    lastname TEXT,
+    email TEXT NOT NULL UNIQUE check (email ~* '^.+@.+\..+$'),
+    password_hash TEXT,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
 COMMENT ON TABLE public.user IS
-'Forum users.';
+'Funani users.';
 
 /*Create post table in public schema*/
 CREATE TABLE public.post (
-    id SERIAL PRIMARY KEY,
+    id UUID PRIMARY KEY default gen_random_uuid(),
     title TEXT,
     body TEXT,
     created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    author_id INTEGER NOT NULL REFERENCES public.user(id)
+    author_id UUID NOT NULL REFERENCES public.user(id)
 );
 
 COMMENT ON TABLE public.post IS
-'Forum posts written by a user.';
+'Funani posts written by a user.';
+
+/*Create MIME type table in public schema*/
+CREATE TABLE public.mime_type (
+    id SERIAL PRIMARY KEY,
+    mime TEXT,
+    is_binary BOOLEAN,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+COMMENT ON TABLE public.mime_type IS
+'Known MIME types.';
+
+/*Create files table in public schema*/
+CREATE TABLE public.file (
+    hash TEXT NOT NULL PRIMARY KEY,
+    mime_id INTEGER REFERENCES public.mime_type(id),
+    size INTEGER NOT NULL,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+COMMENT ON TABLE public.file IS
+'files.';
+
+/*Create root_paths table in public schema*/
+CREATE TABLE public.root_path (
+    id SERIAL PRIMARY KEY,
+    path TEXT NOT NULL,
+    name TEXT,
+    description TEXT,
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+COMMENT ON TABLE public.root_path IS
+'root paths.';
+
+/*Create paths table in public schema*/
+CREATE TABLE public.path (
+    id SERIAL PRIMARY KEY,
+    root_path_id INTEGER REFERENCES public.root_path(id),
+    path TEXT NOT NULL,
+    mtime INTEGER,
+    size INTEGER,
+    hash TEXT
+);
+
+COMMENT ON TABLE public.path IS
+'paths.';
+
+
+/*Create metadata table in public schema*/
+CREATE TABLE public.metadata (
+    id SERIAL PRIMARY KEY,
+    frames INTEGER,
+    width INTEGER,
+    height INTEGER,
+    date_start TIMESTAMP,
+    date_end TIMESTAMP
+);
+
+COMMENT ON TABLE public.metadata IS
+'image or video metadata.';
+
+
+/*
+
+# User Tag TagType TagData1 TagData2
+
+# Hash User TagId
+
+# Hash User Title Description Rating
+
+# TODO:
+# for each metadata file in .meta
+#   - ensure the mime type is in MimeTypes
+#   - find entry in Files table or add it
+#   - foreach src, look in Paths table for a Size/Mtime/RootPathId/Path match
+*/
--- docker/db/init/01-data.sql
+++ docker/db/init/01-data.sql
@@ -1,13 +1,19 @@
 \connect funani;
 
 /*Create some dummy users*/
-INSERT INTO public.user (username) VALUES
-('Benjie'),
-('Singingwolfboy'),
-('Lexius');
+INSERT INTO public.user (id, username, email) VALUES
+('2fc0c398-7d1b-4fcf-8408-7bc838fc2b0c', 'Benjie', 'a@example.com'),
+('498241cf-4a35-428a-a877-be2dd9fc81f1', 'Singingwolfboy', 'b@example.com'),
+('f80cacd4-3550-43b7-9b5d-c630037732dd', 'Lexius', 'c@example.com');
 
 /*Create some dummy posts*/
 INSERT INTO public.post (title, body, author_id) VALUES
-('First post example', 'Lorem ipsum dolor sit amet', 1),
-('Second post example', 'Consectetur adipiscing elit', 2),
-('Third post example', 'Aenean blandit felis sodales', 3);
+('First post example', 'Lorem ipsum dolor sit amet', '2fc0c398-7d1b-4fcf-8408-7bc838fc2b0c'),
+('Second post example', 'Consectetur adipiscing elit', '498241cf-4a35-428a-a877-be2dd9fc81f1'),
+('Third post example', 'Aenean blandit felis sodales', 'f80cacd4-3550-43b7-9b5d-c630037732dd');
+
+INSERT INTO public.mime_type (mime, is_binary) VALUES
+('image/jpeg', TRUE),
+('image/tiff', TRUE),
+('image/png', TRUE),
+('text/plain', FALSE);
