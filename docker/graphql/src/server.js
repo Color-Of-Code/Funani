bb2865116a99623b043a78d003d16842508efa2d
Jaap de Haan <ColorOfCode@googlemail.com>
2021-06-03 15:58:56 +0200

    fix: added pg-simplify-inflector

--- docker/graphql/src/server.js
+++ docker/graphql/src/server.js
@@ -3,15 +3,47 @@ const { postgraphile } = require("postgraphile");
 
 const e = process.env;
 
+const postgraphileCommonOptions = {
+  appendPlugins: [require("@graphile-contrib/pg-simplify-inflector")],
+
+  setofFunctionsContainNulls: false,
+  subscriptions: true,
+  dynamicJson: true,
+  ignoreRBAC: false,
+  ignoreIndexes: false,
+  legacyRelations: "omit",
+
+  jwtSecret: e.JWT_SECRET,
+  jwtPgTypeIdentifier: e.JWT_PG_TYPE,
+  pgDefaultRole: e.DEFAULT_ROLE
+};
+
+const postgraphileDevelopmentOptions = {
+  ...postgraphileCommonOptions,
+  watchPg: true,
+  showErrorStack: "json",
+  extendedErrors: ["hint", "detail", "errcode"],
+  exportGqlSchemaPath: "schema.graphql",
+  graphiql: true,
+  enhanceGraphiql: true,
+  allowExplain(req) {
+    // TODO: customise condition!
+    return true;
+  },
+  enableQueryBatching: true,
+};
+
+const postgraphileProductionOptions = {
+  ...postgraphileCommonOptions,
+  retryOnInitFail: true,
+  extendedErrors: ["errcode"],
+  graphiql: false,
+  enableQueryBatching: true,
+  disableQueryLog: true, // our default logging has performance issues, but do make sure you have a logging system in place!
+};
+
 http
   .createServer(
-    postgraphile(e.DATABASE_URL, "public", {
-      enhanceGraphiql: true,
-      graphiql: true,
-      jwtSecret: e.JWT_SECRET,
-      jwtPgTypeIdentifier: e.JWT_PG_TYPE,
-      pgDefaultRole: e.DEFAULT_ROLE,
-      watchPg: true,
-    })
+    postgraphile(e.DATABASE_URL, "public", postgraphileDevelopmentOptions)
   )
   .listen(e.PORT);
